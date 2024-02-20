d3080438936c35057ffc921e7595f7042829b2f1
Jan Brittenson <bson@rockgarden.net>
2024-02-19 17:12:29 -0800

    Support for D78F0547

--- k0dasm/command.py
+++ k0dasm/command.py
@@ -9,7 +9,7 @@ from k0dasm.disassemble import disassemble
 from k0dasm.memory import Memory
 from k0dasm.trace import Tracer
 from k0dasm.listing import Printer
-from k0dasm.symbols import SymbolTable, D78F0831Y_SYMBOLS
+from k0dasm.symbols import SymbolTable, D78F0547_SYMBOLS
 
 def main():
     if len(sys.argv) != 2:
@@ -22,40 +22,38 @@ def main():
 
     start_address = 0
     entry_points = []
-    hardware_vectors = [ # TODO these are uPD78F0831Y specific
-        0x0000, # RST
-        0x0002, # (unused)
-        0x0004, # INTWDT
-        0x0004, # INTWDT
+    hardware_vectors = [ # TODO these are D78F0547 specific
+        0x0000, # RESET input, POC, LVI, WDT
+        0x0004, # INTLVI
         0x0006, # INTP0
         0x0008, # INTP1
-        0x000a, # INTP2
-        0x000c, # INTP3
-        0x000e, # INTP4
+        0x000A, # INTP2
+        0x000C, # INTP3
+        0x000E, # INTP4
         0x0010, # INTP5
-        0x0012, # INTP6
-        0x0014, # INTP7
-        0x0016, # INTSER0
-        0x0018, # INTSR0
-        0x001a, # INTST0
-        0x001c, # INTCSI30
-        0x001e, # INTCSI31
-        0x0020, # INTIIC0
-        0x0022, # INTC2
-        0x0024, # INTWTNI0
-        0x0026, # INTTM000
-        0x0028, # INTTM010
-        0x002a, # INTTM001
-        0x002c, # INTTM011
-        0x002e, # INTAD00
-        0x0030, # INTAD01
-        0x0032, # (unused)
-        0x0034, # INTWTN0
-        0x0036, # INTKR
-        0x0038, # (unused)
-        0x003a, # (unused)
-        0x003c, # (unused)
-        0x003e,  # BRK_I
+        0x0012, # INTSRE6
+        0x0014, # INTSR6
+        0x0016, # INTST6
+        0x0018, # INTCSI10/INTST0
+        0x001A, # INTTMH1
+        0x001C, # INTTMH0
+        0x001E, # INTTM50
+        0x0020, # INTTM000
+        0x0022, # INTTM010
+        0x0024, # INTAD
+        0x0026, # INTSR0
+        0x0028, # INTWTI
+        0x002A, # INTTM51
+        0x002C, # INTKR
+        0x002E, # INTWT
+        0x0030, # INTP6
+        0x0032, # INTP7
+        0x0034, # INTIIC0/NTDMU
+        0x0036, # INTCSI11
+        0x0038, # INTTM001
+        0x003A, # INTTM011
+        0x003C, # INTACSI
+        0x003E, # BRK
     ]
     callt_vectors = list(range(0x40, 0x7f, 2))
     all_vectors = hardware_vectors + callt_vectors
@@ -64,7 +62,7 @@ def main():
     tracer = Tracer(memory, entry_points, all_vectors, traceable_range)
     tracer.trace(disassemble)
 
-    symbol_table = SymbolTable(D78F0831Y_SYMBOLS)
+    symbol_table = SymbolTable(D78F0547_SYMBOLS) # TODO
     symbol_table.generate(memory, start_address) # xxx should pass traceable_range
 
     printer = Printer(memory,
